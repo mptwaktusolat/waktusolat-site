d3c56a64cade497cdadca76c80e32fc16479a2d7
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2024-01-03 22:03:35 +0800

    :lipstick: Fix UI color

--- components/sections/FeatureSection.jsx
+++ components/sections/FeatureSection.jsx
@@ -49,7 +49,7 @@ const Feature = () => {
             &quot;Hurry to the prayer. Hurry to success&quot;
           </p>
 
-          <p className="mt-6 text-lg leading-8 text-zinc-600">
+          <p className="mt-6 text-lg leading-8 text-zinc-600 dark:text-zinc-300">
             Feature packed while keeping the design clean and minimal
           </p>
         </div>
