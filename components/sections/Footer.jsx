3a3b982c7ff56986ca712d31ba0214a62ac7fa52
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2024-01-04 18:40:53 +0800

    :pencil2: Update copyright year in Footer component

--- components/sections/Footer.jsx
+++ components/sections/Footer.jsx
@@ -46,7 +46,7 @@ const Footer = () => {
           ))}
         </div>
         <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-          &copy; 2023 <a href="https://iqfareez.com">Muhammad Fareez</a>. All
+          &copy; 2024 <a href="https://iqfareez.com">Muhammad Fareez</a>. All
           rights reserved.
         </p>
       </div>
